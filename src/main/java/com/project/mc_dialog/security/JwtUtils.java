63c4425e4bb8918756f2ec64437bbfecb79248d5
GhostASA <artemsvist1809@gmail.com>
2024-08-07 11:04:23 +0400

    delete jwt secret

--- src/main/java/com/project/mc_dialog/security/JwtUtils.java
+++ src/main/java/com/project/mc_dialog/security/JwtUtils.java
@@ -2,42 +2,29 @@ package com.project.mc_dialog.security;
 
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
 @Slf4j
 public class JwtUtils {
 
-    @Value("${app.jwt.secret}")
-    private String secret;
-
     public String getId(String token){
-        return Jwts.parser().verifyWith(createSecretKey(secret)).build()
+        return Jwts.parser().build()
                 .parseSignedClaims(token).getPayload().getId();
 
     }
 
     public String getEmail(String token){
-        return Jwts.parser().verifyWith(createSecretKey(secret))
-                .build().parseSignedClaims(token).getPayload().get("email", String.class);
+        return Jwts.parser().build()
+                .parseSignedClaims(token).getPayload().get("email", String.class);
 
     }
 
     public List<String> getRoles(String token){
-        return Jwts.parser().verifyWith(createSecretKey(secret))
-                .build().parseSignedClaims(token).getPayload().get("roles", List.class);
+        return Jwts.parser().build()
+                .parseSignedClaims(token).getPayload().get("roles", List.class);
 
     }
-
-    public static SecretKey createSecretKey(String secret) {
-        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
-        return new SecretKeySpec(keyBytes, 0, keyBytes.length, "HmacSHA256");
-    }
-
 }
