a0264d01673326a2feaf4d9b2aea14571968358a
GhostASA <artemsvist1809@gmail.com>
2024-08-26 17:51:46 +0400

    websocket v2 test change service

--- src/main/java/com/project/mc_dialog/repository/MessageRepository.java
+++ src/main/java/com/project/mc_dialog/repository/MessageRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
@@ -14,6 +15,8 @@ public interface MessageRepository extends JpaRepository<Message, UUID> {
             "(:partner1 IN (m.conversationPartner1, m.conversationPartner2) AND " +
             ":partner2 IN (m.conversationPartner1, m.conversationPartner2)) " +
             "ORDER BY m.time DESC")
-    Page<Message> findMessages(UUID partner1, UUID partner2, Pageable pageable);
+    Page<Message> findMessages(@Param("partner1") UUID partner1,
+                               @Param("partner2") UUID partner2,
+                               Pageable pageable);
 
 }
