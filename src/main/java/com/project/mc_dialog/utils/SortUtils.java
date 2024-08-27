e348a374dd34b8d9e1e72b0bf93e635852baf927
GhostASA <artemsvist1809@gmail.com>
2024-08-27 12:43:44 +0400

    changes in SortUtils

--- src/main/java/com/project/mc_dialog/utils/SortUtils.java
+++ src/main/java/com/project/mc_dialog/utils/SortUtils.java
@@ -13,7 +13,13 @@ public class SortUtils {
 
         Sort sort = Sort.by(sortFields.get(0));
         for (int i = 1; i < sortFields.size(); i++) {
-            sort = sort.and(Sort.by(sortFields.get(i)));
+            if (sortFields.get(i).equals("desc")){
+                sort = sort.descending();
+            } else if (sortFields.get(i).equals("asc")) {
+                sort = sort.ascending();
+            } else {
+                sort = sort.and(Sort.by(sortFields.get(i)));
+            }
         }
         return sort;
     }
